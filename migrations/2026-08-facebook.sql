48c829e1ecf00740f9320d62ecdf4b994dcf4c0d
Kuba <kozakooo13@gmail.com>
2026-08-13 16:46:07 +0200

    Qualify the Facebook migration with its schema

--- migrations/2026-08-facebook.sql
+++ migrations/2026-08-facebook.sql
@@ -7,9 +7,9 @@
 -- Instagram fetches the file from a URL we sign; Facebook wants the bytes
 -- pushed to it. Both start from the same rendition.
 
-alter table accounts drop constraint accounts_platform_check;
-alter table accounts add constraint accounts_platform_check
+alter table public.accounts drop constraint accounts_platform_check;
+alter table public.accounts add constraint accounts_platform_check
   check (platform in ('instagram', 'tiktok', 'facebook'));
 
-comment on column accounts.platform_user_id is
+comment on column public.accounts.platform_user_id is
   'IG: ig_user_id. Facebook: the Page id. TikTok: open_id.';
